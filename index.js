dcdc650f548bdf49bedd5c84d6b25498c8f31d80
jonathanx111 <jonathanx111@gmail.com>
2020-12-10 01:54:48 -0500

    final

--- index.js
+++ index.js
@@ -10,4 +10,18 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  let t = parseInt(timeString.split(':')[0])
+  if (t < 12) {
+    return "Good Morning"
+  } else if (t >= 17) {
+    return "Good Evening"
+  } else {
+    return "Good Afternoon"
+  }
+}
+
+function displayMessage(msg) {
+  document.getElementById('greeting').innerText = msg
+}
 /* Write your implementation of displayMessage() */
